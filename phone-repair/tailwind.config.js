70fcf2fd79cb7a64d6ab683a8c048a0474088f52
ChukwudiNebo <chukwudineboch@gmail.com>
2023-12-08 23:43:36 -0800

    still working on single artist page

--- phone-repair/tailwind.config.js
+++ phone-repair/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        secondary:"#D3D3D3"
+
+      }
+    },
   },
   plugins: [],
 };
