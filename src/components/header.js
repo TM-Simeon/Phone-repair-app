68ac891ee52f1760a7636a4db1e7e9b58b62f36a
ChukwudiNebo <chukwudineboch@gmail.com>
2024-01-11 15:57:38 -0800

    worked on the navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,52 +1,94 @@
 import React, { useState } from 'react';
-import Image from '../icons/logo.svg'
 import { NavLink } from 'react-router-dom';
+import Image from '../icons/logo.svg';
+import harmbuger from '../icons/hamburger.svg';
 import './header.css';
 import './footer.css';
 
 const Header = () => {
   const [navbarOpen, setNavbar] = useState(false);
   return (
-    <nav className='navbar'>
-      <NavLink to='/' exact className='logo' activeClassName='active-link'>
-        <img src={Image} alt='logo' />
-      </NavLink>
-      <div className='nav-links'>
-        {/* i added font-family and font-weight to the links below */}
-        <NavLink
-          to='/'
-          exact
-          className='nav-link font-body font-[400]'
-          activeClassName='active-link'
-        >
-          Home
+    <div className='relative'>
+      <nav className='navbar'>
+        <NavLink to='/' exact className='logo' activeClassName='active-link'>
+          <img src={Image} alt='logo' />
         </NavLink>
-        <NavLink
-          to='/technician'
-          className='nav-link font-body font-[400]'
-          activeClassName='active-link'
+        {/* Desktop navbar */}
+        <div className='nav-links hidden lg:mx-auto lg:flex lg:items-center '>
+          {/* i added font-family and font-weight to the links below */}
+          <NavLink
+            to='/'
+            exact
+            className='nav-link font-body font-[400]'
+            activeClassName='active-link'
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to='/technician'
+            className='nav-link font-body font-[400]'
+            activeClassName='active-link'
+          >
+            Technician
+          </NavLink>
+          <NavLink
+            to='/support'
+            className='nav-link'
+            activeClassName='active-link font-body font-[400]'
+          >
+            Support
+          </NavLink>
+        </div>
+        <div
+          style={{
+            transition: 'all .5s',
+            visibility: navbarOpen ? 'visible' : 'hidden',
+            left: navbarOpen ? '225px' : '-200px',
+          }}
+          className='absolute right-0 bg-[#EFF0FC] rounded-[10px] lg:hidden w-[40%] top-[65px]'
         >
-          Technician
-        </NavLink>
-        <NavLink
-          to='/support'
-          className='nav-link'
-          activeClassName='active-link font-body font-[400]'
+          {/* mobile navbar  */}
+          {navbarOpen && (
+            <div className='nav-links flex flex-col justify-end py-[19px] px-[15px]'>
+              {/* i added font-family and font-weight to the links below */}
+              <NavLink
+                to='/'
+                exact
+                className='nav-link font-body font-[400]'
+                activeClassName='active-link'
+              >
+                Home
+              </NavLink>
+              <div className='py-1'>
+                <NavLink
+                  to='/technician'
+                  className='nav-link font-body font-[400]'
+                  activeClassName='active-link'
+                >
+                  Technician
+                </NavLink>
+              </div>
+              <NavLink
+                to='/support'
+                className='nav-link'
+                activeClassName='active-link font-body font-[400]'
+              >
+                Support
+              </NavLink>
+            </div>
+          )}
+        </div>
+        <div
+          className='hamburger lg:hidden ml-auto'
+          onClick={() => setNavbar(!navbarOpen)}
         >
-          Support
-        </NavLink>
-      </div>
-      <div
-        className='hamburger d-sm-block d-md-block d-lg-block d-xl-none'
-        onClick={() => setNavbar(!navbarOpen)}
-      >
-        <div className={!navbarOpen ? 'bar' : 'bar one'}></div>
-        <div className={!navbarOpen ? 'bar' : 'bar two'}></div>
-        <div className={!navbarOpen ? 'bar' : 'bar three'}></div>
-      </div>
-    </nav>
+          <div className={!navbarOpen ? 'bar' : 'bar one'}></div>
+          <div className={!navbarOpen ? 'bar' : 'bar two'}></div>
+          <div className={!navbarOpen ? 'bar' : 'bar three'}></div>
+        </div>
+      </nav>
+    </div>
   );
 };
 
 export default Header;
-
