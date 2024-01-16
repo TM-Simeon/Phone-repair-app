5bbae8a2887975423f3234fcb765c828caed1404
ChukwudiNebo <chukwudineboch@gmail.com>
2024-01-16 05:31:53 -0800

    still working on some fixes

--- src/components/find-a-technician/FindATechnician.js
+++ src/components/find-a-technician/FindATechnician.js
@@ -1,42 +1,209 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Input from './input/Input';
 import Textarea from './textarea/Textarea';
+import dropdownarrow from '../../images/dropdownarrow.svg';
 
 const FindATechnician = () => {
   const [note, setNote] = useState(false);
+  const [dropDown, setDropDown] = useState(false);
+  const [dropDownIssue, setDropDownIssue] = useState(false);
+  const dropDownFunction = () => {
+    setDropDown(!dropDown);
+  };
+  const dropDownIssueFunction = () => {
+    setDropDownIssue(!dropDownIssue);
+  };
   const noteFunction = () => {
     setNote(!note);
   };
+  const [logIn, setLogIn] = useState({
+    email: '',
+    password: '',
+  });
+  const onInputChange = (e) => {
+    setLogIn({
+      ...logIn,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  // Initialize state, checking localStorage for a stored value
+  const [isChecked, setChecked] = useState(() => {
+    const storedValue = localStorage.getItem('checkboxState');
+    return storedValue ? JSON.parse(storedValue) : false;
+  });
+
+  // Update localStorage when the state changes
+  useEffect(() => {
+    localStorage.setItem('checkboxState', JSON.stringify(isChecked));
+  }, [isChecked]);
+
+  // Handle checkbox change
+  const handleCheckboxChange = () => {
+    setChecked(!isChecked);
+  };
   return (
     <div>
+      {/* inputType="email" inputName="email" inputPlaceholder="Email address"
+                inputValue={logIn.firstName}
+                onChange={onInputChange} */}
       <form className='w-full lg:w-[603px] mx-auto'>
         <div>
           <div className='mt-6'>
-            <Input inputPlaceholder={'Your current location'} />
+            <Input
+              inputPlaceholder={'Your current location'}
+              onChange={onInputChange}
+            />
           </div>
           <div className='mt-6 flex flex-col gap-y-5'>
-            <Input inputPlaceholder={"Technician's location"} />
+            <Input
+              inputPlaceholder={"Technician's location"}
+              onChange={onInputChange}
+            />
           </div>
-          <div className='mt-6 grid grid-cols-12 lg:gap-5 items-center'>
+          <div className='mt-6 grid grid-cols-12 lg:gap-5 items-center relative'>
             <div className='col-span-12 lg:col-span-6'>
-              <Input inputPlaceholder={'Speciality'} />
+              <div
+                className='flex align-center justify-between text-[#6F6F6F] placeholder-[#6F6F6F] border border-[#6F6F6F] text-[14px] rounded-full lg:pl-[20px] font-body w-full p-[9px_16px_10px_16px]'
+                onClick={dropDownFunction}
+              >
+                Speciality
+                <img
+                  src={dropdownarrow}
+                  alt='img'
+                  className={`w-[34px] h-[20px] ${
+                    dropDown ? 'rotate-180' : 'rotate-0'
+                  }`}
+                />
+              </div>
             </div>
             <div className='col-span-12 lg:col-span-6 mt-6 lg:mt-0'>
-              <Input inputPlaceholder={'phone model (Optional)'} />
+              <Input
+                inputPlaceholder={'phone model (Optional)'}
+                onChange={onInputChange}
+              />
+            </div>
+            {/* dropdown */}
+            <div
+              // style={{
+              //   transition: 'all .5s',
+              // }}
+              className='absolute top-[35px] w-full lg:w-[48%] z-20'
+            >
+              {dropDown && (
+                <div className='border border-[#6F6F6F] mt-4 px-8 py-4 rounded-lg bg-white'>
+                  <div className='flex items-center gap-[15px]'>
+                    <input
+                      type='checkbox'
+                      id='card'
+                      name='card'
+                      value='card'
+                      checked={isChecked}
+                      onChange={handleCheckboxChange}
+                    />
+                    <label
+                      for='speciality'
+                      className='text-sm font-body text-[#6F6F6F]'
+                    >
+                      {' '}
+                      Hardware
+                    </label>
+
+                    <br></br>
+                  </div>
+                  <div className='flex items-center gap-[15px]'>
+                    <input type='checkbox' id='card' name='card' value='card' />
+                    <label
+                      for='card'
+                      className='text-sm font-body text-[#6F6F6F]'
+                    >
+                      {' '}
+                      Software
+                    </label>
+                    <br></br>
+                  </div>
+                  <div className='flex items-center gap-[15px]'>
+                    <input type='checkbox' id='card' name='card' value='card' />
+                    <label
+                      for='card'
+                      className='text-sm font-body text-[#6F6F6F]'
+                    >
+                      {' '}
+                      General
+                    </label>
+                    <br></br>
+                  </div>
+                </div>
+              )}
             </div>
           </div>
           <p className='font-body text-lg mt-4'>Describe your phone issue</p>
-          <div className='mt-4'>
-            <Input inputPlaceholder={'Hardware'} />
+          <div className='mt-4 relative'>
+            <div
+              onClick={dropDownIssueFunction}
+              className='flex align-center justify-between text-[#6F6F6F] placeholder-[#6F6F6F] border border-[#6F6F6F] text-[14px] rounded-full lg:pl-[20px] font-body w-full p-[9px_16px_10px_16px]'
+            >
+              Hardware
+              <img
+                src={dropdownarrow}
+                alt='img'
+                className={`w-[34px] h-[20px] ${
+                  dropDownIssue ? 'rotate-180' : 'rotate-0'
+                }`}
+              />
+            </div>
+            <div className='absolute top-[35px] w-full lg:w-[48%]'>
+              {dropDownIssue && (
+                <div className='border border-[#6F6F6F] mt-4 px-8 py-4 rounded-lg bg-white'>
+                  <div className='flex items-center gap-[15px]'>
+                    <input
+                      type='checkbox'
+                      id='card'
+                      name='card'
+                      value='card'
+                      checked={isChecked}
+                      onChange={handleCheckboxChange}
+                    />
+                    <label
+                      for='speciality'
+                      className='text-sm font-body text-[#6F6F6F]'
+                    >
+                      {' '}
+                      Hardware
+                    </label>
+
+                    <br></br>
+                  </div>
+                  <div className='flex items-center gap-[15px]'>
+                    <input type='checkbox' id='card' name='card' value='card' />
+                    <label
+                      for='card'
+                      className='text-sm font-body text-[#6F6F6F]'
+                    >
+                      {' '}
+                      Software
+                    </label>
+                    <br></br>
+                  </div>
+                </div>
+              )}
+            </div>
           </div>
         </div>
         <div className='mt-6'>
-          <div
-            onClick={noteFunction}
-            className='w-[100%] p-[10px_24px_10px_24px] bg-white text-[#6F6F6F] border border-[#413E3A] text-[14px] lg:text-[16px] rounded-full text-center'
-          >
-            Leave a note
-          </div>
+          {note ? (
+            <Textarea
+              inputPlaceholder={'Leave a note'}
+              onChange={onInputChange}
+            />
+          ) : (
+            <div
+              className='w-[100%] p-[10px_24px_10px_24px] bg-white text-[#6F6F6F] border border-[#413E3A] text-[14px] lg:text-[16px] rounded-full text-center'
+              onClick={noteFunction}
+            >
+              Leave a note
+            </div>
+          )}
           {/* <div className='mt-4 mb-8'>{note && <Textarea />}</div> */}
         </div>
         <div className='text-end mt-4'>
--- src/components/technician.js
+++ src/components/technician.js
@@ -8,11 +8,11 @@ const Technician = () => {
   return (
     <div className='bg-[#FFFFF]'>
       <div className='container max-w-7xl mx-auto px-5 pt-10'>
-        <div className='text-xl lg:text-[40px] leading-[2.5rem] text-[#3D3D3D] font-heading'>
+        <div className='text-xl lg:text-[35px] leading-[2rem] text-[#3D3D3D] font-heading'>
           Find a technician
         </div>
         <div className='mb-[100px]'>
-          <div className='lg:flex lg:flex-wrap lg:justify-between items-center'>
+          <div className='lg:flex lg:flex-wrap lg:justify-between items-start'>
             <div className=''>
               <FindATechnician />
             </div>
